ef5b384c9c83ed687353e66d028af18c7f94aa4c
twotheg <twotheg@gmail.com>
2026-09-11 16:58:12 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,14 +21,13 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full overflow-hidden">
       <body className="h-full overflow-hidden bg-[#0b0f17] select-none touch-none m-0 p-0">
-        {children}
-        {/* Vercel 에러를 방지하기 위해 body 안쪽에 안전하게 애드센스 삽입 */}
         <Script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4424569297437395"
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
+        {children}
       </body>
     </html>
   );
